b4d77380dc984862d39093fb7165ead9dce96be3
Rajat Talwar <cuterajat26@gmail.com>
2018-11-15 11:38:25 +0530

    update build

--- project.js
+++ project.js
@@ -1,3 +1,14 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = require("marko-starter").projectConfig({
-  name: "marko-starter-demo" // Optional, but added here for demo purposes
+  name: "marko-starter-demo", // Optional, but added here for demo purposes
+  lassoConfig: {
+    bundlingEnabled: isProduction,
+    fingerprintsEnabled: isProduction,
+    urlPrefix:"/ustadji",    
+    minifyJS: false,
+    plugins: [
+        'lasso-marko'
+    ]
+}
 });
