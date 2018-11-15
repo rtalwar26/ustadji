2f6d9502b2e9289bc770bb806d046794b4d0be3a
Rajat Talwar <cuterajat26@gmail.com>
2018-11-15 13:21:52 +0530

    update build

--- src/components/relative-link/component.js
+++ src/components/relative-link/component.js
@@ -1,9 +1,9 @@
 const isGithubPublish = (process.env.GITHUB_PUBLISH === 'true');
 module.exports = class {
     onInput(input,out) {
-        
+        let isRelative = input.href && (input.href.charAt(0) === "/");
         const packageConfig = JSON.parse(require("fs").readFileSync("./package.json","utf8"));
-        input.href = isGithubPublish ? `${packageConfig.baseurl}${input.href}` : input.href;        
+        input.href = isGithubPublish && isRelative? `${packageConfig.baseurl}${input.href}` : input.href;        
     }
     
 }
