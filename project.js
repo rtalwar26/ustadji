bfce722201d6ed9822d43448d02aa2b07ee9719d
Rajat Talwar <cuterajat26@gmail.com>
2018-11-15 12:13:47 +0530

    update build

--- project.js
+++ project.js
@@ -3,9 +3,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = require("marko-starter").projectConfig({
   name: "ustad-chhote-rahimat-khan", // Optional, but added here for demo purposes
   lassoConfig: {
+    outputDir: "dist/static",
     bundlingEnabled: isProduction,
     fingerprintsEnabled: isProduction,
-    urlPrefix:"/ustadji",    
+    urlPrefix:"/ustadji/static",    
     minifyJS: false,
     plugins: [
         'lasso-marko'
